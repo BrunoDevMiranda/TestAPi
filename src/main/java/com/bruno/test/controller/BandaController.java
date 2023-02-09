693ed928e027d565f028dcbc4b50692f5af362fd
BrunoDevMiranda <mirandabsm.dev@gmail.com>
2023-02-09 13:58:25 -0300

    Adicionando service ao controller

--- src/main/java/com/bruno/test/controller/BandaController.java
+++ src/main/java/com/bruno/test/controller/BandaController.java
@@ -1,6 +1,8 @@
 package com.bruno.test.controller;
 
 import com.bruno.test.data.Banda;
+import com.bruno.test.service.BandService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/{band}")
 public class BandaController {
+    @Autowired
+    private BandService service;
     @GetMapping(value = "/{id}")
     public ResponseEntity<Banda>findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new Banda(1,"Metallica","HeavyMetal"))
-;    }
+        return ResponseEntity.ok().body(service.findById(id));
+   }
 }
