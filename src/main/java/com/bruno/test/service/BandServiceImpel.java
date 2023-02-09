93397641019ca54da2e61d2f2e78ad47855d59f7
BrunoDevMiranda <mirandabsm.dev@gmail.com>
2023-02-09 15:00:07 -0300

    Classe service impel atualizada

--- src/main/java/com/bruno/test/service/BandServiceImpel.java
+++ src/main/java/com/bruno/test/service/BandServiceImpel.java
@@ -1,6 +1,7 @@
 package com.bruno.test.service;
 
 import com.bruno.test.data.Banda;
+import com.bruno.test.exceptions.ObjectNotFoundException;
 import com.bruno.test.repository.BandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,6 @@ public class BandServiceImpel implements BandService {
     @Override
     public Banda findById(Integer id) {
         Optional<Banda> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(()->new ObjectNotFoundException("Banda não foi encontrada"));
     }
 }
