22af51aeba1df0c65f22b76946d25fc0f1be539a
BrunoDevMiranda <brunomiranda.dev@gmail.com>
2023-02-09 12:11:23 -0300

    Iniciando Projeto APITEST

---
+++ src/main/java/com/bruno/test/StartAPiApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartAPiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bruno/test/TestAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
